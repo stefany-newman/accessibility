a07573b418fddfd6586e840b49b72a553b740ad4
Stefany Newman <stef@stefanynewman.com>
2025-05-15 15:02:15 -0500

    Added a lot of code

--- forms/scripts/main.js
+++
@@ -1,5 +0,0 @@
-const firstHeading = document.querySelector(".heading");
-    
-document.addEventListener("DOMContentLoaded", () => firstHeading.focus());
-// Removes tabindex=-1 from the heading due to potentional bugs
-firstHeading.addEventListener("blur", () => firstHeading.removeAttribute("tabindex"));
