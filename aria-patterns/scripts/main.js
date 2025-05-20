d0fb845d791dfc0a42315f9a955a8e4582611d80
Stefany Newman <stef@stefanynewman.com>
2025-05-20 10:37:50 -0500

    aria selected manipulation

--- aria-patterns/scripts/main.js
+++ aria-patterns/scripts/main.js
@@ -1,1 +1,22 @@
-const 
+const tabs = document.querySelectorAll(".tab");
+
+const toggleAttributeValue = (element, attribute, value) => {
+    let customBoolean = "false";
+    if (element.getAttribute(attribute) === 'true') {
+        customBoolean = false;
+      } else {
+        customBoolean = true;
+      }
+    element.setAttribute(attribute, customBoolean);
+}
+
+
+console.log(tabs);
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", (e) => {
+
+        toggleAttributeValue(tab, "aria-selected", 'true');
+        console.log(e.currentTarget.getAttribute("aria-selected"));
+    })
+});
