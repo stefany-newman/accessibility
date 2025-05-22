2e595fd1fa4ef74f8e5b3ee1a2e4b0e7ec00e875
Stefany Newman <stef@stefanynewman.com>
2025-05-22 15:15:46 -0500

    Code that works but it's low quality

--- aria-patterns/scripts/main.js
+++ aria-patterns/scripts/main.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelectorAll(".tab");
+const tabPanelContent = document.querySelectorAll(".tabpanel-content");
 
-const toggleAttributeValue = (element, attribute, value) => {
+
+const toggleAttributeValue = (element, attribute) => {
     let customBoolean = "false";
     if (element.getAttribute(attribute) === 'true') {
         customBoolean = false;
@@ -9,14 +11,23 @@ const toggleAttributeValue = (element, attribute, value) => {
       }
     element.setAttribute(attribute, customBoolean);
 }
-
-
-console.log(tabs);
-
-tabs.forEach(tab => {
+tabs.forEach((tab, index) => {
     tab.addEventListener("click", (e) => {
-
+        let previouslySelectedTab = document.querySelector(".tab[aria-selected='true'");
+        console.log(previouslySelectedTab.id.slice(-1));
+        if(e.currentTarget.getAttribute("aria-selected") === "false"){
+            toggleAttributeValue(previouslySelectedTab, "aria-selected");
         toggleAttributeValue(tab, "aria-selected", 'true');
+        tabPanelContent[index].toggleAttribute("hidden");
+        e.currentTarget.setAttribute("tabindex", "0");
         console.log(e.currentTarget.getAttribute("aria-selected"));
+        
+        previouslySelectedTab.setAttribute("tabindex", "-1");
+        console.log(tabPanelContent);
+        tabPanelContent[previouslySelectedTab.id.slice(-1)-1].toggleAttribute("hidden");
+
+        }else{
+            console.log("can't disable itself");
+        }
     })
 });
