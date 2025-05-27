abbf0958e460f7a8e1f295b6df6843ea1ea9ffe2
Stefany Newman <stef@stefanynewman.com>
2025-05-27 15:57:43 -0500

    Improved code efficency and readability

--- aria-patterns/scripts/main.js
+++ aria-patterns/scripts/main.js
@@ -1,8 +1,8 @@
 const tabList = document.querySelector("#tablist");
 const tabs = document.querySelectorAll(".tab");
 const tabPanelContent = document.querySelectorAll(".tabpanel-content");
-
-console.log(tablist);
+let selectedTab = document.querySelector(".tab[aria-selected='true'");
+let selectedTabContent = document.querySelector("#tabpanel-content-1");
 
 const toggleAttributeValue = (element, attribute) => {
     let customBoolean = "false";
@@ -14,6 +14,21 @@ const toggleAttributeValue = (element, attribute) => {
     element.setAttribute(attribute, customBoolean);
 }
 
+const unSelectTab = (element) => {
+    element.setAttribute("aria-selected", "false");
+    element.setAttribute("tabindex", "-1");
+}
+const selectTab = (element) => {
+    element.setAttribute("aria-selected", "true");
+    element.setAttribute("tabindex", "0");
+}
+
+const toggleTabContentlVisibility = (selectedTab, tabContent) => {
+    if(!null(tabContent.getAttribute("hidden"))){
+        tabContent.toggleAttribute("hidden");
+    }
+}
+
 
 // document.addEventListener("keydown", (e) => {
 //     if(e.code === "ArrowLeft")
@@ -22,24 +37,37 @@ const toggleAttributeValue = (element, attribute) => {
 //     }
 // });
 
- tabs.forEach((tab, index) => {
-    tab.addEventListener("click", (e) => {
-        let previouslySelectedTab = document.querySelector(".tab[aria-selected='true'");
-        console.log(previouslySelectedTab.id.slice(-1));
-        if(e.currentTarget.getAttribute("aria-selected") === "false"){
-            toggleAttributeValue(previouslySelectedTab, "aria-selected");
-        toggleAttributeValue(tab, "aria-selected", 'true');
-        tabPanelContent[index].toggleAttribute("hidden");
-        e.currentTarget.setAttribute("tabindex", "0");
-        console.log(e.currentTarget.getAttribute("aria-selected"));
+//  tabs.forEach((tab, index) => {
+//     tab.addEventListener("click", (e) => {
+//         let previouslySelectedTab = document.querySelector(".tab[aria-selected='true'");
+//         console.log(previouslySelectedTab.id.slice(-1));
+//         if(e.currentTarget.getAttribute("aria-selected") === "false"){
+//             toggleAttributeValue(previouslySelectedTab, "aria-selected");
+//         toggleAttributeValue(tab, "aria-selected", 'true');
+//         tabPanelContent[index].toggleAttribute("hidden");
+//         e.currentTarget.setAttribute("tabindex", "0");
+//         console.log(e.currentTarget.getAttribute("aria-selected"));
         
-        previouslySelectedTab.setAttribute("tabindex", "-1");
-        console.log(tabPanelContent);
-        tabPanelContent[previouslySelectedTab.id.slice(-1)-1].toggleAttribute("hidden");
+//         previouslySelectedTab.setAttribute("tabindex", "-1");
+//         console.log(tabPanelContent);
+//         tabPanelContent[previouslySelectedTab.id.slice(-1)-1].toggleAttribute("hidden");
 
-        }else{
-            console.log("can't disable itself");
-        }
-    })
+//         }else{
+//             console.log("can't disable itself");
+//         }
+//     })
+// });
+
+tabList.addEventListener("click", (e) => {
+    if(e.target.getAttribute("role") === "tab" && e.target.getAttribute("aria-selected") === "false"){
+       unSelectTab(selectedTab);
+       selectedTab = e.target;
+       const tabNumber = selectedTab.id.substring(selectedTab.id.indexOf("-")+1);
+       selectTab(selectedTab);
+       selectedTabContent.toggleAttribute("hidden");
+       selectedTabContent = document.querySelector("#tabpanel-content-"+tabNumber);
+       selectedTabContent.toggleAttribute("hidden");
+       
+    }
 });
 
